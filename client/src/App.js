f39542e2abeb367267f0f7f560d92377649450f5
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-01-17 20:12:50 -0500

    started project

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import Route from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,6 +14,10 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route path= '/' component ={MovieList} />
+
+      <Route path = '/movies' component ={About} />
+
     </div>
   );
 };
